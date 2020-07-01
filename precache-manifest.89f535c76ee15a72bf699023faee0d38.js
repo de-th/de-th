61b35fb14ec08aeb24a4e3a81244471c5e1b7c40
de-th <thilinadevdun@gmail.com>
2020-07-01 22:49:26 +0530

    initial commit

---
+++ precache-manifest.89f535c76ee15a72bf699023faee0d38.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8cc501d431bbf35994",
+    "url": "/css/app.0f116802.css"
+  },
+  {
+    "revision": "91b0fea7350ebecf9055",
+    "url": "/css/chunk-vendors.eaec7fcd.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "557d7b0e54c5d36fb74ff19ee359c485",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a8cc501d431bbf35994",
+    "url": "/js/app.7f483917.js"
+  },
+  {
+    "revision": "91b0fea7350ebecf9055",
+    "url": "/js/chunk-vendors.83539020.js"
+  },
+  {
+    "revision": "70ee01ed71e1fdf2ede8cb941fd3e1a4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5e6d701e25d78d614c1f81faccee553e",
+    "url": "/sent.jpg"
+  }
+]);
